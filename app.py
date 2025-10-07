ac31234ea740c07de38582095d21907fe39bcc6a
root <root@ip-172-31-20-83.ec2.internal>
2025-10-07 14:19:45 +0000

    Updated requirements with gunicorn and modified app for Gunicorn support

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Jenkins CI/CD with Flask!"
+    return "Hello, World!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run()
 
